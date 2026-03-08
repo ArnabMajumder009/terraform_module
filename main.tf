b321e8573cd0c40e2c1f671a0895b06a75bd1a5e
ArnabMajumder009 <majumderarnab009@gmail.com>
2026-03-08 10:26:05 +0530

    commit module

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.mycidr
+  tags = {
+    Name = local.tag
+  }
+}
+
+output "vpcid" {
+  value = aws_vpc.myvpc.id
+}
+
+output "cidr" {
+  value = aws_vpc.myvpc.cidr_block
+  #sensitive = true
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "mycidr" {
+  type      = string
+  default   = "10.0.0.0/16"
+  #sensitive = true
+}
